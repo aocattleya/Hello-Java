9350295d7f0e15f20bce8ecd54aa9b9f0c36edd5
aocattleya <aocattleya@yahoo.co.jp>
2018-10-12 17:06:24 +0900

    5章の練習問題を追加

---
+++ スッキリわかるjava入門/src/Chapter5/Question2.java
@@ -0,0 +1,17 @@
+package Chapter5;
+
+public class Question2 {
+
+	public static void main(String[] args) {
+		String title = "来週の件";
+		String address = "a-more@yapon.co.jp";
+		String text = "来週は飲み会があります、しかし私はお酒が飲めません。";
+		email(title, address, text);
+	}
+
+	public static void email(String title, String address, String text) {
+		System.out.println("「" + address + "」に、以下のメールを送信しました");
+		System.out.println("件名：「" + title + "」");
+		System.out.println("本文：「" + text + "」");
+	}
+}
---
+++ スッキリわかるjava入門/src/Chapter5/Question4.java
@@ -0,0 +1,23 @@
+package Chapter5;
+
+public class Question4 {
+
+	public static void main(String[] args) {
+
+		double calcTriangleArea = calcTriangleArea(10, 5);
+		System.out.println("三角形の面積：" + calcTriangleArea + "平方cm");
+
+		double CiecleArea = calcCircleArea(5);
+		System.out.println("円の面積：" + CiecleArea + "平方cm");
+	}
+
+	public static double calcTriangleArea(double bottom, double height) {
+		double area = bottom * height / 2;
+		return area;
+	}
+
+	public static double calcCircleArea(double radius) {
+		double area = radius * radius * 3.14;
+		return area;
+	}
+}
